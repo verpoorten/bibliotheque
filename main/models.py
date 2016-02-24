e3c30769a759e42655eb046a643b243e3e4f319b
Leila Verpoorten <leila.verpoorten@uclouvain.be>
2016-02-24 15:14:12 +0100

    Ajout de la notion d'user dans la personne

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Personne(models.Model):
     nom    = models.CharField(max_length = 100, blank = False, null = False)
     prenom = models.CharField(max_length = 100, blank = False, null = False)
-
+    user        = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self):
         return self.nom.upper() + ", " + self.prenom
 
